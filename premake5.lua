41cef3c397149aabca9e762c96a8130aec56f0f0
NishiOwO <nishi@nishi.boats>
2025-05-09 04:44:16 +0900

    seems to work

--- premake5.lua
+++ premake5.lua
@@ -18,45 +18,15 @@ for _,v in ipairs({
 	"trans",
 	"utils"
 }) do
-		kind("StaticLib")
-		files("src/" .. v .. "/*.c")
+	kind("StaticLib")
+	files("src/" .. v .. "/*.c")
+end
+for _,v in ipairs({
+	"md5"
+}) do
+	files("modules/" .. v .. "/*.c")
 end
 	includedirs("include")
 	includedirs("modules/md5")
 	targetdir("lib/%{cfg.buildcfg}/%{cfg.platform}")
 	defines("HAVE_CONFIG_H")
-for _,v in ipairs({
-	"app",
-	"cache",
-	"core",
-	"dir",
-	"file",
-	"ftp",
-	"gopher",
-	"html",
-	"http",
-	"init",
-	"mime",
-	"mux",
-	"news",
-	"stream",
-	"telnet",
-	"trans",
-	"utils"
-}) do
-	project("www" .. v)
-		kind("StaticLib")
-		files("src/" .. v .. "/*.c")
-		includedirs("include")
-		includedirs("modules/md5")
-		targetdir("lib/%{cfg.buildcfg}/%{cfg.platform}")
-		defines("HAVE_CONFIG_H")
-end
-for _,v in ipairs({
-	"md5"
-}) do
-	project(v)
-		kind("StaticLib")
-		files("modules/" .. v .. "/*.c")
-		targetdir("lib/%{cfg.buildcfg}/%{cfg.platform}")
-end
